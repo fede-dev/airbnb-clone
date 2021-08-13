d731569d302e6b033c1f25aa80f27b949ea188e7
fede-dev <fedenriquez@hotmail.com>
2021-08-13 15:38:16 -0300

    front/ok

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It´s a pretty awsome clone</p>
+                <p>Referrals accepted</p>
+                <p>Airbnb contact</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Find a place</p>
+                <p>Presents</p>
+                <p>Choice a place</p>
+                <p>Hundreds of people</p>
+                <p>Join now</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help center</p>
+                <p>Trust and safety</p>
+                <p>Youtube channel</p>
+                <p>Facebook</p>
+                <p>Instagram</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
